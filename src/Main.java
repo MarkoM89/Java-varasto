485354a08e4b1ac74cd9e5e8ca32ffd00da07094
Marko Mustonen <marko.mustonen.89@gmail.com>
2025-03-14 17:00:06 +0200

    Tuotu Eclipse-projekti VSCodeen

--- src/Main.java
+++ src/Main.java
@@ -17,36 +17,36 @@ public class Main {
         
         
         Properties connConfig = new Properties();
-        connConfig.setProperty("user", "root");
-        connConfig.setProperty("password", "T13t0k4!?t4");
+        connConfig.setProperty("user", "kÃ¤yttÃ¤jÃ¤");
+        connConfig.setProperty("password", "salasana");
 
 
         
         while (toiminto != 4) {
         	
         	/*
-Päätoiminto 1: Varaston hallinta
+Pï¿½ï¿½toiminto 1: Varaston hallinta
 		Toiminto 1: Varastosaldo
-		Toiminto 2: Osta lisää tuotetta varastoon
+		Toiminto 2: Osta lisï¿½ï¿½ tuotetta varastoon
 
 
-Päätoiminto 2: Tuotehallinta
-		Toiminto 1: Lisää tuote
+Pï¿½ï¿½toiminto 2: Tuotehallinta
+		Toiminto 1: Lisï¿½ï¿½ tuote
 		Toiminto 2: Poista tuote
 		Toiminto 3: Tulosta tuotteet
 		
-Päätoiminto 3: Seuranta
+Pï¿½ï¿½toiminto 3: Seuranta
 		Toiminto 1: Selaa kuitteja
 		Toiminto 2: Etsi kuitti
 
 		
-Päätoiminto 4: Poistu ohjelmasta
+Pï¿½ï¿½toiminto 4: Poistu ohjelmasta
 
         	 
         	 */
         	
-        	System.out.print("\nPäätoiminto 1: Varaston hallinta\nPäätoiminto 2: Tuotehallinta\nPäätoiminto 3: Seuranta\n"
-        			+ "Päätoiminto 4: Poista ohjelmasta\n");
+        	System.out.print("\nPï¿½ï¿½toiminto 1: Varaston hallinta\nPï¿½ï¿½toiminto 2: Tuotehallinta\nPï¿½ï¿½toiminto 3: Seuranta\n"
+        			+ "Pï¿½ï¿½toiminto 4: Poista ohjelmasta\n");
         	toiminto = lukija.nextInt();
         	lukija.nextLine();
         	
@@ -63,7 +63,7 @@ Päätoiminto 4: Poistu ohjelmasta
 		    		while (toiminto != 4) {
 		    		System.out.println("Tuotteiden hallinta");
 		
-		    		System.out.print("\nToiminto 1: Lisää tuote\nToiminto 2: Poista tuote\nToiminto 3: Tulosta tuotteet\n"
+		    		System.out.print("\nToiminto 1: Lisï¿½ï¿½ tuote\nToiminto 2: Poista tuote\nToiminto 3: Tulosta tuotteet\n"
 		    				+ "Toiminto 4: Poistu tuotehallinnasta\n");
 		    			
 		    			
@@ -74,11 +74,11 @@ Päätoiminto 4: Poistu ohjelmasta
 		            		case 1:
 		            			System.out.println("Tuotteen nimi:");
 		            			nimi = lukija.nextLine();
-		            			System.out.println("Tuotteen yksikköhinta:");
+		            			System.out.println("Tuotteen yksikkï¿½hinta:");
 		            			tuoteHinta = lukija.nextDouble();
 		
-		            			try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/kokeilutietokanta", connConfig)) {
-		    		             	PreparedStatement stmt = conn.prepareStatement("INSERT INTO tuote (tuotenimi, yksikköhinta) VALUES (?, ?)");
+		            			try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/pankki_kauppa", connConfig)) {
+		    		             	PreparedStatement stmt = conn.prepareStatement("INSERT INTO tuote (tuotenimi, yksikkï¿½hinta) VALUES (?, ?)");
 		    		            	stmt.setString(1, nimi);
 		    		            	stmt.setDouble(2, tuoteHinta);
 		    		            	stmt.executeQuery(); 
@@ -92,13 +92,13 @@ Päätoiminto 4: Poistu ohjelmasta
 		            		break;
 		    				case 2:
 
-		    	    			System.out.println("Mikä tuote poistetaan? syötä luku 0, jos et halua poistaa mitään");
+		    	    			System.out.println("Mikï¿½ tuote poistetaan? syï¿½tï¿½ luku 0, jos et halua poistaa mitï¿½ï¿½n");
 		    	    			tuotetunniste = lukija.nextInt();
 		    	    			lukija.nextLine();
 		    	    			
 		    	    			
 		    	    			if(tuotetunniste != 0) {
-		            			try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/kokeilutietokanta", connConfig)) {
+		            			try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/pankki_kauppa", connConfig)) {
 		    		             	PreparedStatement stmt = conn.prepareStatement("DELETE FROM tuote WHERE tuotetunniste = ?");
 		    		            	stmt.setInt(1, tuotetunniste);
 		    		            	stmt.executeQuery(); 
@@ -117,12 +117,12 @@ Päätoiminto 4: Poistu ohjelmasta
 								
 				    			System.out.println("Tulosta tuotteet");
 				    			
-				    	        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/kokeilutietokanta", connConfig)) {
+				    	        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/pankki_kauppa", connConfig)) {
 				    	            try (Statement stmt = conn.createStatement()) {
 				    	                try (ResultSet tuotetiedot = stmt.executeQuery("SELECT * FROM tuote")) {
 				    	                    while (tuotetiedot.next()) {
 				    	                    	
-				    	                    	System.out.println(tuotetiedot.getString("tuotetunniste")+ " " +tuotetiedot.getString("tuotenimi")+ " " +tuotetiedot.getString("yksikköhinta")+ "€");
+				    	                    	System.out.println(tuotetiedot.getString("tuotetunniste")+ " " +tuotetiedot.getString("tuotenimi")+ " " +tuotetiedot.getString("yksikkï¿½hinta")+ "ï¿½");
 				    	                    }
 				    	                }
 				    	            }
@@ -150,12 +150,12 @@ Päätoiminto 4: Poistu ohjelmasta
     		case 5:
     			System.out.println("Tulosta korttien tiedot");
     			
-    	        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/kokeilutietokanta", connConfig)) {
+    	        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/pankki_kauppa", connConfig)) {
     	            try (Statement stmt = conn.createStatement()) {
     	                try (ResultSet tilitiedot = stmt.executeQuery("SELECT * FROM pankki")) {
     	                    while (tilitiedot.next()) {
     	                    	
-    	                    	System.out.println(tilitiedot.getString("tunniste")+ " " +tilitiedot.getString("nimi")+ " " +tilitiedot.getString("saldo")+ "€");
+    	                    	System.out.println(tilitiedot.getString("tunniste")+ " " +tilitiedot.getString("nimi")+ " " +tilitiedot.getString("saldo")+ "ï¿½");
     	                    }
     	                }
     	            }
@@ -165,12 +165,12 @@ Päätoiminto 4: Poistu ohjelmasta
     	        
     			System.out.println("\n\nTulosta kuitit");
     			
-    	        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/kokeilutietokanta", connConfig)) {
+    	        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/pankki_kauppa", connConfig)) {
     	            try (Statement stmt = conn.createStatement()) {
     	                try (ResultSet kuittitiedot = stmt.executeQuery("SELECT * FROM kuitti")) {
     	                    while (kuittitiedot.next()) {
     	                    	
-    	                    	System.out.println(kuittitiedot.getInt("kuittitunnus")+ " " +kuittitiedot.getTimestamp("osto_aika")+ " " +kuittitiedot.getDouble("kokonaishinta")+ "€");
+    	                    	System.out.println(kuittitiedot.getInt("kuittitunnus")+ " " +kuittitiedot.getTimestamp("osto_aika")+ " " +kuittitiedot.getDouble("kokonaishinta")+ "ï¿½");
     	                    }
     	                }
     	            }
